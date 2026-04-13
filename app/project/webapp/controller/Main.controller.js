18d17ab470b4933de4b1ca3cd756c741109bec0a
Javier <paltanlobeidajavier@iesblasdeotero.com>
2026-04-13 16:49:21 +0200

    Añadido segunda view y cuerpo del backend creado

--- app/project/webapp/controller/Main.controller.js
+++ app/project/webapp/controller/Main.controller.js
@@ -4,11 +4,39 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("project.controller.Main", {
+
         onInit() {
         },
 
-        changeViewToSegunda: function() {
+        onNavMain: function () {
+            this.getOwnerComponent().getRouter().navTo("RouteMain");
+        },
+
+        onNavNews: function () {
             this.getOwnerComponent().getRouter().navTo("RouteSegunda");
         },
+        onNavBanners: function () {
+            // this.getOwnerComponent().getRouter().navTo("RouteBanners");
+        },
+        onNavMissions: function () {
+            // this.getOwnerComponent().getRouter().navTo("RouteMissions");
+        },
+        onNavCharacters: function () {
+            // this.getOwnerComponent().getRouter().navTo("RouteCharacters");
+        },
+        onNavEvents: function () {
+            // this.getOwnerComponent().getRouter().navTo("RouteEvents");
+        },
+        onNavItems: function () {
+            // this.getOwnerComponent().getRouter().navTo("RouteItems");
+        },
+
+        onOpenProfile:  function () { },
+        onOpenSettings: function () { },
+        onChangeLang:   function () { },
+
+        onOpenPrivacy: function () { },
+        onOpenCookies: function () { },
+        onOpenTerms:   function () { },
     });
 });
--- app/project/webapp/controller/Segunda.controller.js
+++ app/project/webapp/controller/Segunda.controller.js
@@ -4,10 +4,30 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("project.controller.Segunda", {
+
         onInit() {
         },
 
-        changeViewToMain: function() {
+        onNavMain: function () {
+            this.getOwnerComponent().getRouter().navTo("RouteMain");
+        },
+
+        onNavNews:       function () { },
+        onNavBanners:    function () { },
+        onNavMissions:   function () { },
+        onNavCharacters: function () { },
+        onNavEvents:     function () { },
+        onNavItems:      function () { },
+
+        onOpenProfile:  function () { },
+        onOpenSettings: function () { },
+        onChangeLang:   function () { },
+
+        onOpenPrivacy: function () { },
+        onOpenCookies: function () { },
+        onOpenTerms:   function () { },
+
+        changeViewToMain: function () {
             this.getOwnerComponent().getRouter().navTo("RouteMain");
         },
     });
